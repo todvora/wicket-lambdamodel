1e6512de411e652807cb8e17b15a8ab3e9122fbd
Tomas Dvorak <todvora@gmail.com>
2014-08-12 16:20:42 +0200

    added FunctionalInterface annotations

--- src/main/java/cz/tomasdvorak/wicket/model/SerializableConsumer.java
+++ src/main/java/cz/tomasdvorak/wicket/model/SerializableConsumer.java
@@ -6,5 +6,6 @@ import java.util.function.Consumer;
 /**
  * Same interface as {@link java.util.function.Consumer}, but implementing {@link java.io.Serializable}
  */
+@FunctionalInterface
 public interface SerializableConsumer<T> extends Consumer<T>, Serializable {
 }
--- src/main/java/cz/tomasdvorak/wicket/model/SerializableSupplier.java
+++ src/main/java/cz/tomasdvorak/wicket/model/SerializableSupplier.java
@@ -6,5 +6,6 @@ import java.util.function.Supplier;
 /**
  * Same interface as {@link java.util.function.Supplier}, but implementing {@link java.io.Serializable}
  */
+@FunctionalInterface
 public interface SerializableSupplier<T> extends Supplier<T>, Serializable {
 }
